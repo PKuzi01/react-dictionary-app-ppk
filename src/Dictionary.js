365d91390e36fe9c0a8ff06bd3237a81d0fec347
Philisizwe Kuzwayo <cre8storeese@gmail.com>
2023-09-18 18:30:21 +0200

    Added the SheCodes Dictionary API to project

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import axios from "axios";
 
 function Dictionary() {
     let [keyword, setKeyword] = useState("");
@@ -7,10 +8,18 @@ function Dictionary() {
         setKeyword(event.target.value);
     }
 
+    function handleResponse(response) {
+        console.log(response.data);
+    }
+
     function search(event) {
         event.preventDefault();
         alert(`Searching for the definition of ${keyword}...`);
-    }
+        
+        const apiKey = `9a96e3865c186c9fbo4aaef0cdb0e0dt`;
+        const apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
+        axios.get(apiUrl).then(handleResponse);
+    } 
 
     return (
         <div>
