d621fa87ae1f148deda5d6c3d4c39fb58c565bc0
Philisizwe Kuzwayo <cre8storeese@gmail.com>
2023-09-18 18:05:00 +0200

    Added the hero

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import Dictionary from "./Dictionary.js"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <h1>
+          Dictionary
+        </h1>
+        <h2>
+          What word do you want to look up?
+        </h2>
+        <Dictionary />
+      </div>
     </div>
   );
 }
