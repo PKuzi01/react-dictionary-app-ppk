0a9040022363d07d99b5305506c6430d339ff352
Philisizwe Kuzwayo <cre8storeese@gmail.com>
2023-09-22 14:47:43 +0200

    Add synonyms

---
+++ src/Synonyms.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+function Synonyms(props){
+    if (props.synonyms) {
+        return (
+            <div className="Synonyms">
+                <strong>Synonyms:{" "}</strong>
+                {props.synonyms.map(function (synonym, index) {
+                    return <span key={index}>{synonym}{" "}</span>;
+                })}
+            </div>
+        );
+    } else {
+        return null;
+    }
+    
+}
+
+export default Synonyms;
