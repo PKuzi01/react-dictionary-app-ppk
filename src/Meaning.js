f8e6702d0960747db162c06cd9f8f40ea22c2322
Philisizwe Kuzwayo <cre8storeese@gmail.com>
2023-09-27 13:03:17 +0200

    Fixed margin for defintion separateness

--- src/Meaning.js
+++ src/Meaning.js
@@ -2,16 +2,16 @@ import React from "react";
 import Synonyms from "./Synonyms";
 import Antonyms from "./Antonyms";
 import Example from "./Example";
+import "./Meaning.css"
 
 function Meaning(props) {
     //console.log(props.meaning);
     return (
-        <div>
-            <h4>{props.meaning.partOfSpeech}</h4> 
+        <div className="Meaning">
+            <h3>{props.meaning.partOfSpeech}</h3> 
             <p><strong>Definition:</strong> {props.meaning.definition}</p>
             <Example example={props.meaning.example} /> 
             <Synonyms synonyms={props.meaning.synonyms} />
-            <br />
             <Antonyms antonyms={props.meaning.antonyms} />
         </div>
         
